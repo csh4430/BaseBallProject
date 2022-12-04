c0f9e1c9d4fd1a7c0bc2f7e15a5c27a75132e389
csh4430 <snghun6889@gmail.com>
2022-12-04 16:46:08 +0900

    swing

--- Assets/Scripts/Player/Hitter/HitterSwing.cs
+++ Assets/Scripts/Player/Hitter/HitterSwing.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using Unit;
 using UnityEngine;
 
 public class HitterSwing : PlayerBehaviour
 {
+    private Sequence seq;
     private GameObject _battingAimObject;
+    private GameObject _batObject;
     private Vector3 _originalMousePoint;
-    
     private Vector3 _originalAimPoint;
     public override void Awake()
     {
@@ -16,6 +18,7 @@ public class HitterSwing : PlayerBehaviour
 
     public override void Start()
     {
+        _batObject = GameObject.Find("Bat");
         _battingAimObject = GameObject.Find("BattingAim");
     }
 
@@ -33,11 +36,11 @@ public class HitterSwing : PlayerBehaviour
         {
             Vector3 mousePosition = Camera.main.ScreenToWorldPoint(mousPos); 
             Vector3 dir = _originalMousePoint - mousePosition;
-            Debug.Log(mousePosition);
+            
             _battingAimObject.transform.position = _originalAimPoint - dir;
         }
         if (Input.GetMouseButtonUp(0))
-        {            
+        {
             Swing();
         }
     }
@@ -50,5 +53,13 @@ public class HitterSwing : PlayerBehaviour
     public void Swing()
     {
         Player.AddState(PlayerState.Swing);
+        seq = DOTween.Sequence();
+        seq.AppendInterval(1f);
+        seq.Append(_batObject.transform.DOLocalRotate(new Vector3(0, 0, 90 * (_battingAimObject.transform.localPosition.y + 0.5f)), 1f));
+        seq.Append(_batObject.transform.DOLocalRotate(new Vector3(0, 0, 0), 0.5f));
+        seq.AppendCallback(() =>
+        {
+            seq.Kill();
+        });
     }
 }
